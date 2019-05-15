b368f61b759da6388d7f76cf1c9851299d56d8ef
Thiago Butignon <thiagobutignon@gmail.com>
2019-05-15 20:57:48 -0300

    correção da autenticação!

--- Login/Login/LoginViewController.swift
+++ Login/Login/LoginViewController.swift
@@ -17,9 +17,9 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var jellyImage: UIImageView!
     @IBOutlet weak var appNameTitle: UILabel!
     
-    let containerSeahorseView = UIView(frame: CGRect(x: 240, y: 180, width: 0, height: 0))
-    let containerTitleView = UIView(frame: CGRect(x: 100, y: 140, width: 174, height: 63))
-    let containerJellyView = UIView(frame: CGRect(x: -217, y: 489, width: 648, height: 345))
+    var containerSeahorseView = UIView(frame: CGRect(x: 240, y: 180, width: 0, height: 0))
+    var containerTitleView = UIView(frame: CGRect(x: 100, y: 140, width: 174, height: 63))
+    var containerJellyView = UIView(frame: CGRect(x: -217, y: 489, width: 648, height: 345))
     var dataUser: DataUser = DataUser(data: [])
     
     override func viewDidLoad() {
@@ -36,11 +36,21 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
             self.seahorseImage.center = self.containerSeahorseView.center
             self.appNameTitle.center = self.containerTitleView.center
             self.jellyImage.center = self.containerJellyView.center
+            self.containerSeahorseView = UIView(frame: CGRect(x: 280, y: 140, width: 0, height: 0))
+            self.containerTitleView = UIView(frame: CGRect(x: 110, y: 150, width: 174, height: 63))
+            self.containerJellyView = UIView(frame: CGRect(x: -190, y: 450, width: 648, height: 345))
         })
     }
     
     func authAnimate() {
-        
+        UIView.animate(withDuration: 3.0, animations: { () -> Void in
+            self.seahorseImage.center = self.containerSeahorseView.center
+            self.appNameTitle.center = self.containerTitleView.center
+            self.jellyImage.center = self.containerJellyView.center
+            self.containerSeahorseView = UIView(frame: CGRect(x: 240, y: 180, width: 0, height: 0))
+            self.containerTitleView = UIView(frame: CGRect(x: 100, y: 140, width: 174, height: 63))
+            self.containerJellyView = UIView(frame: CGRect(x: -217, y: 489, width: 648, height: 345))
+        })
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -65,10 +75,11 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
             if dataUser.data.contains(where: {$0.email == user.email }) == true{
                 errorLabel.text = "Email and password are registered!"
                 view.shake(duration: 2.0)
-
+                
             } else {
                 save(user)
                 errorLabel.text = "Sucess!"
+                authAnimate()
             }
         } catch (let error) {
             errorLabel.text = (error as! ValidationError).message
@@ -79,14 +90,15 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     func login() {
         do {
             let email = try emailTextField.validatedText(validationType: ValidatorType.email)
-            let password = try emailTextField.validatedText(validationType: ValidatorType.password)
+            let password = try passwordTextField.validatedText(validationType: ValidatorType.password)
             let user: User = User(email: email, password: password)
             
-            if dataUser.data.contains(where: {$0.email == user.email}) && dataUser.data.contains(where: {$0.password == user.password}) {
+            if dataUser.data.contains(where: {$0.email == user.email}) == true && dataUser.data.contains(where: {$0.password == user.password}) == true {
                 errorLabel.text = "You are authenticated"
+                animate()
             } else {
-                errorLabel.text = "You are authenticated"
-
+                errorLabel.text = "Email or password are wrong!"
+                view.shake(duration: 2.0)
             }
             
         } catch (let error) {
@@ -102,12 +114,16 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     
     @IBAction func signUp(_ sender: UIButton) {
         validate()
+        self.view.endEditing(true)
+        print(dataUser.data)
     }
     
     
     
     @IBAction func signIn(_ sender: UIButton) {
-        
+        login()
+        self.view.endEditing(true)
+        print(dataUser.data)
     }
     
 }

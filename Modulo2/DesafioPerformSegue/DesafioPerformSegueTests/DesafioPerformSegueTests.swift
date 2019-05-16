72c71f75f87024950b81dfe32c34d63c961f4e68
Thiago Butignon <thiagobutignon@gmail.com>
2019-05-16 19:53:57 -0300

    Projeto criado para Desafio Perform Segue

---
+++ Modulo2/DesafioPerformSegue/DesafioPerformSegueTests/DesafioPerformSegueTests.swift
@@ -0,0 +1,34 @@
+//
+//  DesafioPerformSegueTests.swift
+//  DesafioPerformSegueTests
+//
+//  Created by Thiago B Claramunt on 16/05/19.
+//  Copyright © 2019 Thiago B Claramunt. All rights reserved.
+//
+
+import XCTest
+@testable import DesafioPerformSegue
+
+class DesafioPerformSegueTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

490e3006d87f1bc2c7449d87a10ef0afe52c7aab
Thiago Butignon <thiagobutignon@gmail.com>
2019-05-16 19:50:57 -0300

    segue apreendedidas

---
+++ Modulo2/PerformSegue/PerformSegue/AmarelaViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AmarelaViewController.swift
+//  PerformSegue
+//
+//  Created by Thiago B Claramunt on 16/05/19.
+//  Copyright © 2019 Thiago B Claramunt. All rights reserved.
+//
+
+import UIKit
+
+class AmarelaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func voltarButton(_ sender: UIButton) {
+        self.dismiss(animated: true)
+    }
+}

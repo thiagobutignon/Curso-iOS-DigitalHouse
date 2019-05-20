18f463d18e96ac750992ec3aed71f4116a210d7c
Thiago Butignon <thiagobutignon@gmail.com>
2019-05-20 19:07:10 -0300

    tableview do additem renderizando

--- Modulo2/DesafioTableView/DesafioTableView/AddItemTableViewController.swift
+++ Modulo2/DesafioTableView/DesafioTableView/AddItemTableViewController.swift
@@ -9,18 +9,28 @@
 import UIKit
 
 class AddItemTableViewController: UITableViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        navigationItem.largeTitleDisplayMode = .never
     }
-
+    
+    
+    @IBAction func cancel(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    
+    @IBAction func done(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+    }
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
+        return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return 1
     }
-
+    
 }

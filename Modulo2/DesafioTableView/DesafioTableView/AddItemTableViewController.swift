9ec1faa44bc6225f7aea20c2b3d943090aa2699b
Thiago Butignon <thiagobutignon@gmail.com>
2019-05-20 18:06:27 -0300

    adicionado segue no checklist e criado o arquivo additemtableviewcontroller

---
+++ Modulo2/DesafioTableView/DesafioTableView/AddItemTableViewController.swift
@@ -0,0 +1,26 @@
+//
+//  AddItemTableViewController.swift
+//  DesafioTableView
+//
+//  Created by Thiago B Claramunt on 20/05/19.
+//  Copyright © 2019 Thiago B Claramunt. All rights reserved.
+//
+
+import UIKit
+
+class AddItemTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+}

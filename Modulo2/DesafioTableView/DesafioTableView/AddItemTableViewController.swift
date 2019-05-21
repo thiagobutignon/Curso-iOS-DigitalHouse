3c2b2c355256fdef10a0815685078edad73dc199
Thiago Butignon <thiagobutignon@gmail.com>
2019-05-21 01:46:02 -0300

    adicionado o keyboard first respond

--- Modulo2/DesafioTableView/DesafioTableView/AddItemTableViewController.swift
+++ Modulo2/DesafioTableView/DesafioTableView/AddItemTableViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class AddItemTableViewController: UITableViewController {
     
+
+    @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.largeTitleDisplayMode = .never
@@ -23,14 +25,15 @@ class AddItemTableViewController: UITableViewController {
     
     @IBAction func done(_ sender: Any) {
         navigationController?.popViewController(animated: true)
+        print("Contents of the text field \(textField.text)")
     }
     
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+    override func viewWillAppear(_ animated: Bool) {
+        textField.becomeFirstResponder()
     }
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return nil
     }
-    
+
 }

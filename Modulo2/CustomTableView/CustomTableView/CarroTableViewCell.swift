73557f9f6480ee1ca58e9a8e91b642bb6489851a
Thiago Butignon <thiagobutignon@gmail.com>
2019-05-21 19:35:18 -0300

    criado o CarroTableViewCell e o XIB

---
+++ Modulo2/CustomTableView/CustomTableView/CarroTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CarroTableViewCell.swift
+//  CustomTableView
+//
+//  Created by Thiago B Claramunt on 21/05/19.
+//  Copyright © 2019 Thiago B Claramunt. All rights reserved.
+//
+
+import UIKit
+
+class CarroTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}

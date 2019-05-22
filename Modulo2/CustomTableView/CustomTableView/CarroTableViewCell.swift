4a9eadee518752afbdc2a5a662a9cdde2aa6deca
Thiago Butignon <thiagobutignon@gmail.com>
2019-05-21 21:52:25 -0300

    adicionado as imagens, refactoring no config cell

--- Modulo2/CustomTableView/CustomTableView/CarroTableViewCell.swift
+++ Modulo2/CustomTableView/CustomTableView/CarroTableViewCell.swift
@@ -9,10 +9,24 @@
 import UIKit
 
 class CarroTableViewCell: UITableViewCell {
-
+    @IBOutlet weak var imagemCarro: UIImageView!
+    @IBOutlet weak var nomeCarro: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+    }
+    
+    func configCell(nome: String, imagem: String) {
+        self.nomeCarro.text = nome
+        self.imagemCarro.image = UIImage(named: imagem)
+    }
+    
+    func configCell(carro: Carro) {
+        self.nomeCarro.text = carro.nome
+        if let _imagemNome = carro.nomeImagem {
+            self.imagemCarro.image = UIImage(named: _imagemNome)
+        }
     }
     
 }
--- Modulo2/CustomTableView/CustomTableView/ListaCarrosVC.swift
+++ Modulo2/CustomTableView/CustomTableView/ListaCarrosVC.swift
@@ -9,13 +9,39 @@
 import UIKit
 
 class ListaCarrosVC: UIViewController {
-
+     let item = CarroLista()
+    
     @IBOutlet weak var carrosTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.carrosTableView.register(UINib(nibName: "CarroTableViewCell", bundle: nil), forCellReuseIdentifier: "CarroTableViewCell")
+        
+        self.carrosTableView.delegate = self
+        self.carrosTableView.dataSource = self
     }
-
-
 }
 
+extension ListaCarrosVC: UITableViewDataSource, UITableViewDelegate {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.item.listaCarros.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: CarroTableViewCell = tableView.dequeueReusableCell(withIdentifier: "CarroTableViewCell", for: indexPath) as! CarroTableViewCell
+        
+        cell.configCell(carro: CarroLista.init().listaCarros[indexPath.row])
+        
+        return cell
+    }
+    
+    
+    
+}
